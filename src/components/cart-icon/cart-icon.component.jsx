79e7d5956adfcb44e95b8eb327281c42a6a7f59a
tgoslee <60710670+tgoslee@users.noreply.github.com>
2020-05-17 23:12:44 -0400

    adding history to cart dropdown and routing to checkout page

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import {toggleCartHidden} from "../../redux/cart/cart.actions";
 import './cart-icon.styles.scss';
 import {ReactComponent as ShoppingIcon} from '../../assests/shoppingbag.svg';
 import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
+import {createStructuredSelector} from "reselect";
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
@@ -17,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = (state) => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {connect} from 'react-redux';
+import {createStructuredSelector} from "reselect";
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {auth} from "../../firebase/firebase.utils";
 import logo from '../../assests/taglogo.png';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/user/user.selectors";
 
 const Header = ({currentUser, hidden}) => (
     <div className='header'>
@@ -31,9 +34,9 @@ const Header = ({currentUser, hidden}) => (
     </div>
 )
 
-const mapStateToProps = ({user: {currentUser},cart:{hidden} }) => ({
-    currentUser,
-    hidden
-})
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
 
 export default connect(mapStateToProps)(Header);
